48d2ea70a00d917c9af2a059a1284570b837907e
Ignacio <7141944+ignaciocastro@users.noreply.github.com>
2025-09-17 17:43:26 -0300

    pageclip integration

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ const ContactForm = () => {
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          <form method="post" action="https://send.pageclip.co/{yourSiteKey}" className="space-y-6">
+          <form method="post" action="https://send.pageclip.co/jD3KNbJ9bl1YFXHkTTAsSvKvPYvE9VzK/contact-form" className="space-y-6">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div>
                 <Input 
