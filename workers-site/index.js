7f19b01cf6d1bd9ae5ece016e7b78c35cdaaf84e
Ignacio <7141944+ignaciocastro@users.noreply.github.com>
2025-09-17 16:33:04 -0300

    add github workers

---
+++ workers-site/index.js
@@ -0,0 +1,63 @@
+import {
+  getAssetFromKV,
+  mapRequestToAsset,
+} from "@cloudflare/kv-asset-handler";
+
+/**
+ * The DEBUG determines if the asset handler will cache requests
+ * on edge locations, or bypass the cache and go straight to the origin.
+ */
+const DEBUG = false;
+
+addEventListener("fetch", (event) => {
+  try {
+    event.respondWith(
+      handleEvent(event).catch((e) => {
+        return new Response(e.message || e.toString(), {
+          status: 500,
+        });
+      })
+    );
+  } catch (e) {
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 500,
+        })
+      );
+    }
+    event.respondWith(new Response("Internal Error", { status: 500 }));
+  }
+});
+
+async function handleEvent(event) {
+  const url = new URL(event.request.url);
+  let options = {};
+
+  try {
+    if (DEBUG) {
+      // customize caching
+      options.cacheControl = {
+        bypassCache: true,
+      };
+    }
+    return await getAssetFromKV(event, options);
+  } catch (e) {
+    // if an error is thrown try to serve the asset at 404.html
+    if (!DEBUG) {
+      try {
+        let notFoundResponse = await getAssetFromKV(event, {
+          mapRequestToAsset: (req) =>
+            new Request(`${new URL(req.url).origin}/404.html`, req),
+        });
+
+        return new Response(notFoundResponse.body, {
+          ...notFoundResponse,
+          status: 404,
+        });
+      } catch (e) {}
+    }
+
+    return new Response(e.message || e.toString(), { status: 500 });
+  }
+}
