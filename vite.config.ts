8ed0eba63faa4e5f4845ac80a3e5004b4f27d33b
Ignacio <7141944+ignaciocastro@users.noreply.github.com>
2025-09-18 07:11:18 -0300

    trying this css tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    optimizeCssModules()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
